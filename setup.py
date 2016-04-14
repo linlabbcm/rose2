dbd41163f64584389d9cc3052643078941d6074e
LinLab Admin <linlabbcm@gmail.com>
2016-04-14 11:00:34 -0500

    adding license

--- setup.py
+++ setup.py
@@ -9,14 +9,13 @@ setup(
 	version='1.0.0',
 	description='ROSE2 python package',
 	long_description='PROGRAM TO STITCH TOGETHER REGIONS TO FORM ENHANCERS, MAP READ DENSITY TO STITCHED REGIONS,AND RANK ENHANCERS BY READ DENSITY TO DISCOVER SUPER-ENHANCERS',
-	url='',
-	author='',
-	license='',
+	url='https://github.com/linlabbcm/rose2',
+	download_url = 'https://github.com/linlabbcm/rose2/tarball/1.0.0',
+	license='LICENSE.txt',
 
-	classifiers=[
+	classifiers=[],
 
-		],
-	keywords='stitched regions enhancer reads',
+	keywords=['bioinformatics','super-enhancers'],
 
 	packages=find_packages(),
 
