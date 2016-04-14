6a861c0a6d39b6b138561f5b1790b79ed11ab4f9
LinLab Admin <linlabbcm@gmail.com>
2016-04-14 11:09:43 -0500

    tarball fix

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
 	description='ROSE2 python package',
 	long_description='PROGRAM TO STITCH TOGETHER REGIONS TO FORM ENHANCERS, MAP READ DENSITY TO STITCHED REGIONS,AND RANK ENHANCERS BY READ DENSITY TO DISCOVER SUPER-ENHANCERS',
 	url='https://github.com/linlabbcm/rose2',
-	download_url = 'https://github.com/linlabbcm/rose2/tarball/1.0.0',
+	download_url = 'https://github.com/linlabbcm/rose2/tarball/v1.0.0',
 	license='LICENSE.txt',
 
 	classifiers=[],
