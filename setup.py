3d20ceb1212f54900d073e0a7a4f2bcb66ac6965
Jost Vrabič Koren <jvrakor@gmail.com>
2019-10-04 13:33:32 -0500

    Bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ from setuptools import setup, find_packages
 
 setup(
 	name='rose2',
-	version='1.0.0',
+	version='1.1.0',
 	description='ROSE2 python package',
 	long_description='PROGRAM TO STITCH TOGETHER REGIONS TO FORM ENHANCERS, MAP READ DENSITY TO STITCHED REGIONS,AND RANK ENHANCERS BY READ DENSITY TO DISCOVER SUPER-ENHANCERS',
 	url='https://github.com/linlabbcm/rose2',
-	download_url = 'https://github.com/linlabbcm/rose2/tarball/v1.0.0',
+	download_url = 'https://github.com/linlabbcm/rose2/tarball/v1.1.0',
 	
 
 	classifiers=[],
